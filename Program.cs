f4307c8fc37f13feb512b71e0175bfe0e636a722
Axel Petersson <axel.818@gmail.com>
2024-02-26 11:11:53 +0100

    refactor and add beginnign to guesser

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ using System.IO;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
+    {
+        Guesser();
+    }
+
+    static string GetRandomWord()
     {
         string[] words = File.ReadAllLines("data/Words.txt");
 
@@ -11,12 +16,24 @@ class Program
         {
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, words.Length);
-
-            Console.WriteLine("Random Word: " + words[randomIndex]);
+            return words[randomIndex];
         }
         else
         {
             Console.WriteLine("The file is empty or does not exist.");
+            return "hej hej ";
+        }
+    }
+
+    static void Guesser()
+    {
+        string WordToGuess = GetRandomWord();
+        Console.WriteLine(WordToGuess);
+        int maxAttemptes = 5;
+        char[] wordDisplay = new char[WordToGuess.Length];
+        for (int i = 0; i < WordToGuess.Length; i++)
+        {
+            wordDisplay[i] = '_';
         }
     }
 }
--- obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
+++ obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e15d86accd33280f27651ce481fc3d6120677de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17cf41c4f7d47b603b0a65c9af2e839b9e9aefbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
