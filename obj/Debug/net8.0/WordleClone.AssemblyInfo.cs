44d2197d7fcf898836f1ea12c65645744a0c23fe
Axel Petersson <axel.818@gmail.com>
2024-02-26 10:32:48 +0100

    second commit

--- obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
+++ obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a0eff2e8570d1086451de58454bc5d7414ad1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
