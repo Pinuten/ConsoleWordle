90fee7eaf245a95437a72851bc3011f3d3cf6e88
Axel Petersson <axel.818@gmail.com>
2024-02-26 12:55:57 +0100

    can guess on letter at a time

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Guesser();
+
     }
 
     static string GetRandomWord()
@@ -36,6 +38,8 @@ class Program
             wordDisplay[i] = '_';
         }
         int attemptsLeft = maxAttemptes;
+
+
         Console.WriteLine("Welcome to Salt Wordle lol lol lol");
         Console.WriteLine($"You have {maxAttemptes} left");
         while (attemptsLeft > 0)
@@ -43,13 +47,40 @@ class Program
             Console.WriteLine("\nCurrent Progress: " + string.Join(" ", wordDisplay));
             Console.Write("Enter a letter: ");
             char guessedLetter = char.ToLower(Console.ReadKey().KeyChar);
+
             if (WordToGuess.Contains(guessedLetter))
             {
+                for (int i = 0; i < WordToGuess.Length; i++)
+                {
+                    if (WordToGuess[i] == guessedLetter)
+                    {
+                        wordDisplay[i] = guessedLetter;
+                        attemptsLeft--;
+                    }
+                }
+
+                if (new string(wordDisplay) == WordToGuess)
+                {
+                    Console.WriteLine("\nCongratulations! You've guessed the word: " + WordToGuess);
+                    break;
+                }
             }
+            else
+            {
+                attemptsLeft--;
+                Console.WriteLine("\nIncorrect guess! Attempts left: " + attemptsLeft);
+            }
+        }
+
+        if (attemptsLeft == 0)
+        {
+            Console.WriteLine("\nSorry, you've run out of attempts. The word was: " + WordToGuess);
         }
     }
 }
-
+//Console.BackgroundColor = ConsoleColor.Blue;
+//Console.ForegroundColor = ConsoleColor.White;
+//Console.WriteLine("White on blue.");
 
 
 
--- obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
+++ obj/Debug/net8.0/WordleClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4307c8fc37f13feb512b71e0175bfe0e636a722")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf8c5d6952cb7dcad2626c983ea5c4f0497e44b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordleClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
